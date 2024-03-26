39b37e32e348580e543fcbf749f7ec8e4fcaf901
banana <banana@ed.ac.uk>
2024-03-26 18:44:58 +0000

    efw

--- r/demo_print.R
+++ r/demo_print.R
@@ -1,1 +1,7 @@
-print("banana-teacher")
+print("banana-teacher studeent")
+
+
+system("git config --global user.email 'banana@ed.ac.uk' ")
+system("git config --global user.name 'banana'")
+
+       
